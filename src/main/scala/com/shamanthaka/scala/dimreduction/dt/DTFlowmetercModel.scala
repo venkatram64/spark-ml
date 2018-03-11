9891f5dd719e1a9735a55c70225a9cfe495cd2c8
venkatram64 <venkat.veerareddy@hotmail.com>
2018-03-11 07:55:09 +0530

    added funtionality

---
+++ src/main/scala/com/shamanthaka/scala/dimreduction/dt/DTFlowmetercModel.scala
@@ -0,0 +1,97 @@
+package com.shamanthaka.scala.dimreduction.dt
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature._
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Shamanthaka on 12/25/2017.
+  */
+object DTFlowmetercModel extends App{
+
+  val sparkSession = SparkSession
+    .builder()
+    .master("local")
+    .appName("DTFlowmetercModel")
+    .getOrCreate()
+
+
+  val data = sparkSession.read.format("libsvm").load("flowmeterc_libsvm_data")
+  //show schema
+  data.printSchema()
+
+  val colnames = data.columns
+  val firstrow = data.head(1)(0)
+  println("\n")
+  println("Example Data Row")
+
+  for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
+  }
+
+  // Index labels, adding metadata to the label column.
+  // Fit on whole dataset to include all labels in index.
+  val labelIndexer = new StringIndexer()
+    .setInputCol("label")
+    .setOutputCol("indexedLabel")
+    .fit(data)
+  // Automatically identify categorical features, and index them.
+  // Set maxCategories so features with > 4 distinct values are treated as continuous.
+  val featureIndexer = new VectorIndexer()
+    .setInputCol("features")
+    .setOutputCol("indexedFeatures")
+    .setMaxCategories(4)
+    .fit(data)
+
+
+
+  // Split the data into training and test sets (30% held out for testing).
+  val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+  // Train a DecisionTree model.
+  val dt = new DecisionTreeClassifier()
+    .setLabelCol("indexedLabel")
+    .setFeaturesCol("indexedFeatures")
+
+  // Convert indexed labels back to original labels.
+  val labelConverter = new IndexToString()
+    .setInputCol("prediction")
+    .setOutputCol("predictedLabel")
+    .setLabels(labelIndexer.labels)
+
+  // Chain indexers and forest in a Pipeline.
+  val pipeline = new Pipeline()
+    .setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+
+  // Train model. This also runs the indexers.
+  val model = pipeline.fit(trainingData)
+
+  model.write.overwrite().save("dtFlowmetercModel");
+
+  val predictions = model.transform(testData)
+
+  predictions.printSchema()
+
+  // Select example rows to display.
+  predictions.select("predictedLabel", "label", "probability", "features").show(300)
+
+  // Select (prediction, true label) and compute test error.
+  val evaluator = new MulticlassClassificationEvaluator()
+    .setLabelCol("indexedLabel")
+    .setPredictionCol("prediction")
+    .setMetricName("accuracy")
+  val accuracy = evaluator.evaluate(predictions)
+
+  println("Test Accuracy = " + accuracy * 100)
+  println("Test Error = " + (1.0 - accuracy) * 100)
+
+  val dtModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+  println("Learned classification forest model:\n" + dtModel.toDebugString)
+
+  sparkSession.stop()
+
+}
---
+++ src/main/scala/com/shamanthaka/scala/dimreduction/lr/LRFlowmetercPrediction.scala
@@ -0,0 +1,47 @@
+package com.shamanthaka.scala.dimreduction.lr
+
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Shamanthaka on 12/27/2017.
+  */
+object LRFlowmetercPrediction extends App{
+
+  val sparkSession = SparkSession
+    .builder()
+    .master("local")
+    .appName("LRFlowmetercPrediction")
+    .getOrCreate()
+
+  val testData = sparkSession.read.format("libsvm").load("flowmeterc_libsvm_data")
+  //show schema
+  testData.printSchema()
+
+  val colnames = testData.columns
+  val firstrow = testData.head(1)(0)
+  println("\n")
+  println("Example Data Row")
+
+  for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
+  }
+
+
+  import sparkSession.implicits._
+
+  val model = PipelineModel.load("lrFlowmetercModel")
+
+
+  val predictions = model.transform(testData)
+
+  predictions.printSchema()
+
+  val predictionAndLabels = predictions.select($"prediction", $"label",$"probability")
+  predictionAndLabels.show(100)
+
+  sparkSession.stop()
+
+}
